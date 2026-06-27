1d274415d62c63742b59f21830836858d65fe174
Yashpal Gevhare <yashugurjar447@gmail.com>
2026-06-27 21:24:14 +0530

    Added MySQL integration with Spring Data JPA

--- src/main/java/com/example/demo/StudentData.java
+++ src/main/java/com/example/demo/StudentData.java
@@ -1,6 +1,18 @@
 package com.example.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
 public class StudentData {
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+
 	
 	private String name;
 	private int  age ;
